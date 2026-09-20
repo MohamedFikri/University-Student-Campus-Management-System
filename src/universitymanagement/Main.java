2e1fc911f0f5003dbbf7278d1b69d68256f5bd7d
Mohammed Fikri <moahmedfikri272@gmail.com>
2026-09-20 11:18:29 +0530

    Initial project setup

---
+++ src/universitymanagement/Main.java
@@ -0,0 +1,9 @@
+package universitymanagement;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("University Student and Campus Management System");
+        System.out.println("Project setup completed successfully!");
+    }
+}
